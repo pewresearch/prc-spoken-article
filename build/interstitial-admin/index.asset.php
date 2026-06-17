9f582c810830bcaf2963996f5735f8d4550a24f5
prcdevgitbot <webdev@pewresearch.org>
2026-06-17 23:06:05 +0000

    See https://github.com/pewresearch/prc-platform/commit/419eea996f99c86f88a6f703559925263ee15244 from refs/tags/v1.9.0

--- build/interstitial-admin/index.asset.php
+++ build/interstitial-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => '14cdf7fae7b8befb50fe');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => '743c71ad3e545579df81');
