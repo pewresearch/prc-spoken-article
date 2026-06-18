d4ed4d2e24b535e8dd510a4660a32dd8e0069156
prcdevgitbot <webdev@pewresearch.org>
2026-06-18 18:38:52 +0000

    See https://github.com/pewresearch/prc-platform/commit/78ae575e96ed8eb6aac68bbd570fd6f388103747 from refs/tags/v1.9.1

--- build/player-block/index.asset.php
+++ build/player-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '83cb0b5d923eb987bca9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '49580f26333e4aa303c5');
--- build/player-trigger-block/index.asset.php
+++ build/player-trigger-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('prc-icons', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-primitives'), 'version' => '70d7289fbc7e512779b5');
+<?php return array('dependencies' => array('prc-icons', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-primitives'), 'version' => '492b21e0dd9c261a9ccd');
